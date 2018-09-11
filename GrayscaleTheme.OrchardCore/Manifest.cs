4868bf0a4f1963909e6db4841bbee49a02cef94e
DannyT <dan@moov2.com>
2018-09-12 00:54:29 +0100

    adding theme

---
+++ GrayscaleTheme.OrchardCore/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "GraysacleTheme.OrchardCore",
+    Author = "Dan Thomas",
+    Website = "https://danny-t.co.uk",
+    Version = "0.0.1",
+    Description = "OrchardCore theme implementation of Grayscale Bootstrap theme by Blackrock Digital"
+)]
